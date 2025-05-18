c6e79a741349efaa4284b93503db28596b3164b0
Alan Santos <alan.santos@usto.re>
2025-05-18 02:20:42 -0300

    :sparkles: feat: criação da tela de login e tela de cadastro

---
+++ src/app/auth/login/login.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule, FormsModule, RouterModule],
+  templateUrl: './login.component.html',
+})
+export class LoginComponent {
+  email = '';
+  password = '';
+}
